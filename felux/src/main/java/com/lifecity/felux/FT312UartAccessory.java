de9fa6796e13270f88816a0fc7a8eb81db98481b
Stephen Caudle <scaudle@doceme.com>
2013-06-19 17:01:35 -0400

    Convert tabs to spaces

--- felux/src/main/java/com/lifecity/felux/FT312UartAccessory.java
+++ felux/src/main/java/com/lifecity/felux/FT312UartAccessory.java
@@ -20,10 +20,10 @@ import android.widget.Toast;
 
 public class FT312UartAccessory
 {
-	private static final String TAG = "FT312UartAccessory";
-	public ParcelFileDescriptor fileDescriptor = null;
-	public FileInputStream inputStream = null;
-	public FileOutputStream outputStream = null;
+    private static final String TAG = "FT312UartAccessory";
+    public ParcelFileDescriptor fileDescriptor = null;
+    public FileInputStream inputStream = null;
+    public FileOutputStream outputStream = null;
 
     public static final byte DATA_BITS_7 = (byte)7;
     public static final byte DATA_BITS_8 = (byte)8;
@@ -40,45 +40,45 @@ public class FT312UartAccessory
     public static final byte FLOW_CONTROL_NONE = (byte)0;
     public static final byte FLOW_CONTROL_CTS_RTS = (byte)1;
 
-	private byte [] writeBuffer = new byte[256];
+    private byte [] writeBuffer = new byte[256];
 
     public void reset() throws IOException  {
         writeBuffer[0] = (byte)64;
         writeUsb(1);
     }
 
-	public void setConfig(int baudRate, byte dataBits, byte stopBits, byte parity,
-			byte flowControl) throws IOException {
-		writeBuffer[0] = (byte)baudRate;
-		writeBuffer[1] = (byte)(baudRate >> 8);
-		writeBuffer[2] = (byte)(baudRate >> 16);
-		writeBuffer[3] = (byte)(baudRate >> 24);
+    public void setConfig(int baudRate, byte dataBits, byte stopBits, byte parity,
+            byte flowControl) throws IOException {
+        writeBuffer[0] = (byte)baudRate;
+        writeBuffer[1] = (byte)(baudRate >> 8);
+        writeBuffer[2] = (byte)(baudRate >> 16);
+        writeBuffer[3] = (byte)(baudRate >> 24);
 
-		writeBuffer[4] = dataBits;
-		writeBuffer[5] = stopBits;
+        writeBuffer[4] = dataBits;
+        writeBuffer[5] = stopBits;
         writeBuffer[6] = parity;
-		writeBuffer[7] = flowControl;
+        writeBuffer[7] = flowControl;
 
-		writeUsb((int)8);
-	}
+        writeUsb((int)8);
+    }
 
-	public void write(byte[] buffer) throws IOException {
-		write(buffer, 0, buffer.length);
-	}
+    public void write(byte[] buffer) throws IOException {
+        write(buffer, 0, buffer.length);
+    }
 
-	public void write(byte[] buffer, int count) throws IOException {
-		write(buffer, 0, count);
-	}
+    public void write(byte[] buffer, int count) throws IOException {
+        write(buffer, 0, count);
+    }
 
-	public void write(byte[] buffer, int offset, int count) throws IOException {
-		count = Math.min(buffer.length, count);
-		count = Math.min(256, count);
+    public void write(byte[] buffer, int offset, int count) throws IOException {
+        count = Math.min(buffer.length, count);
+        count = Math.min(256, count);
 
-		if(offset < count && count > 0) {
-			for(int i = offset; i < count; i++)
-			{
-				writeBuffer[i] = buffer[i];
-			}
+        if(offset < count && count > 0) {
+            for(int i = offset; i < count; i++)
+            {
+                writeBuffer[i] = buffer[i];
+            }
 
             if(count != 64)
             {
@@ -92,15 +92,15 @@ public class FT312UartAccessory
                 writeUsb(1);
             }
         }
-	}
+    }
 
-	private void writeUsb(int count) throws IOException {
+    private void writeUsb(int count) throws IOException {
         if(outputStream != null){
             outputStream.write(writeBuffer, 0, count);
         }
-	}
+    }
 
-	public boolean open(UsbManager manager, UsbAccessory accessory) throws IOException {
+    public boolean open(UsbManager manager, UsbAccessory accessory) throws IOException {
         if (accessory != null) {
             //close();
             if (fileDescriptor == null && inputStream == null && outputStream == null) {
@@ -117,7 +117,7 @@ public class FT312UartAccessory
         }
 
         return false;
-	}
+    }
 
     public void close() throws IOException {
         if(fileDescriptor != null) {
