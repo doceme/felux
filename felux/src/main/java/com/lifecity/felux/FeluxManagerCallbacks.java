c6a9c5741442268e3610397f035a949609c8d766
Stephen Caudle <scaudle@doceme.com>
2013-07-01 17:45:24 -0400

    Add felux manager callback interface

---
+++ felux/src/main/java/com/lifecity/felux/FeluxManagerCallbacks.java
@@ -0,0 +1,10 @@
+package com.lifecity.felux;
+
+public interface FeluxManagerCallbacks<T> {
+    /**
+     * Callback for when an item has been selected.
+     */
+    public void onItemSelected(int position, T item);
+    public void onItemAdded(T item);
+    public void onItemUpdated(T item);
+}
